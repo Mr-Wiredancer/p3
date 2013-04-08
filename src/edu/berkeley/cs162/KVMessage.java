21b255c462e768cd7cf71567b303c2d2390b37a6
Jiahao Li <lijiahao90@gmail.com>
2013-04-08 00:18:16 -0700

    KVMessage now uses xml representation

--- src/edu/berkeley/cs162/KVMessage.java
+++ src/edu/berkeley/cs162/KVMessage.java
@@ -3,17 +3,22 @@ package edu.berkeley.cs162;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.Socket;
 import java.io.*;
-import java.net.*;
 import javax.xml.parsers.*;
-import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
@@ -32,12 +37,12 @@ public class KVMessage {
 
 	//TODO this is now a naive implementation for testing purpose without use of XML
 	public static final String GETTYPE = "getreq";
-  public static final String PUTTYPE = "putreq";
-  public static final String DELTYPE = "delreq";
-  public static final String RESPTYPE = "resp";
+	public static final String PUTTYPE = "putreq";
+	public static final String DELTYPE = "delreq";
+	public static final String RESPTYPE = "resp";
   
-  public static final int MAX_KEY_LENGTH = 256;
-  public static final int MAX_VALUE_LENGTH = 256*1024;
+	public static final int MAX_KEY_LENGTH = 256;
+	public static final int MAX_VALUE_LENGTH = 256*1024;
 
 	private String msgType = null;
 	private String key = null;
@@ -124,82 +129,188 @@ public class KVMessage {
      */
 	public KVMessage(InputStream input) throws KVException {
 	     // TODO: implement me
-		BufferedReader in;
-		in = new BufferedReader(new InputStreamReader(input));
-		try {
-			String msg = in.readLine();
-			System.out.println(Thread.currentThread().getName()+": "+msg);
-			String[] fields = msg.split(",");
-			System.out.println(Thread.currentThread().getName()+": "+fields[0]+";"+fields[1]);
-			if ( fields[0].equals(KVMessage.PUTTYPE) ){
-				this.msgType = fields[0];
-				this.key = fields[1];
-				this.value = fields[2];	
-			}else if( fields[0].equals(KVMessage.GETTYPE)){
-				this.msgType = fields[0];
-				this.key = fields[1];
-			}else if( fields[0].equals(KVMessage.DELTYPE) ){
-				this.msgType = fields[0];
-				this.key = fields[1];
-			}else if(fields[0].equals(KVMessage.RESPTYPE)){
-				this.msgType = fields[0];
-				this.message = fields[1];
-			}else{
-				throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Message format incorrect"));
-			}
-			
-		//this is case b	
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+    	try {
+    		
+    		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    		Document doc = dBuilder.parse(input);
+    		
+    		doc.getDocumentElement().normalize();
+    		
+    		//check doc structure
+    		checkDocStructure(doc);
+    		
+    		Node message = doc.getFirstChild();
+    		
+    		String type = checkKVMessageStructure(message);
+    		this.msgType = type;
+    		
+    		NodeList nodes = message.getChildNodes();
+    		if (type.equals(KVMessage.PUTTYPE)){
+    			String key = checkKeyNode(nodes.item(0));
+    			String val = checkValNode(nodes.item(1));
+    			this.key = key;
+    			this.value = val;
+    		}else if(type.equals(KVMessage.GETTYPE)){
+    			String key = checkKeyNode(nodes.item(0));
+    			this.key = key;
+    		}else if(type.equals(KVMessage.DELTYPE)){
+    			String key = checkKeyNode(nodes.item(0));
+    			this.key = key;
+    		}else{
+    			String msg = checkMessageNode(nodes.item(0));
+    			this.message = msg;
+    		}
+    		
+		} catch (ParserConfigurationException e) {
+			//this should not happen
 			e.printStackTrace();
-			throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Network Error: Could not receive data"));
+		} catch (SAXException e) {
+			//not a valid XML
+			e.printStackTrace();
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+		} catch (IOException e) {
+			//io error
+			e.printStackTrace();
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "Network Error: Could not receive data") );
 		}
+	}
+	
+	private String checkMessageNode(Node messageNode) throws KVException{
+		if (messageNode.getAttributes().getLength()!=0)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		NodeList nodes = messageNode.getChildNodes();
+		if ( nodes.getLength()!=1 || nodes.item(0).getNodeType()!=Document.TEXT_NODE)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		String key = messageNode.getFirstChild().getTextContent();
+		return key;
+	}
+	
+	private String checkKeyNode(Node keyNode) throws KVException{
+		if (keyNode.getAttributes().getLength()!=0)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		NodeList nodes = keyNode.getChildNodes();
+		if ( nodes.getLength()!=1 || nodes.item(0).getNodeType()!=Document.TEXT_NODE)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		String key = keyNode.getFirstChild().getTextContent();
+		if (key.length()>KVMessage.MAX_KEY_LENGTH)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "Oversized key"));
+		return key;
+	}
+	
+	private String checkValNode(Node valNode) throws KVException{
+		if (valNode.getAttributes().getLength()!=0)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		NodeList nodes = valNode.getChildNodes();
+		if ( nodes.getLength()!=1 || nodes.item(0).getNodeType()!=Document.TEXT_NODE)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		String key = valNode.getFirstChild().getTextContent();
+		if (key.length()>KVMessage.MAX_VALUE_LENGTH)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "Oversized value"));
+		return key;
+	}
+	
+	/**
+	 * 
+	 * @param messageNode
+	 * @return KVMessage type
+	 * @throws KVException
+	 */
+	private String checkKVMessageStructure(Node messageNode) throws KVException{
+		NamedNodeMap attrs = messageNode.getAttributes();
 		
-//		DocumentBuilderFactory builderFactory =
-//		        DocumentBuilderFactory.newInstance();
-//		DocumentBuilder builder = null;
-//		try {
-//		    builder = builderFactory.newDocumentBuilder();
-//		} catch (ParserConfigurationException e) {
-//		    e.printStackTrace();  
-//		}
-//		try{
-//			Document dom = builder.parse(input);
-//			if (!isValidMessage(dom)){
-//				this.msgType = KVMessage.RESPTYPE;
-//				this.message = "Message format incorrect";
-//			}
-//			
-//		} catch (SAXException e) {
-//		    this.msgType = KVMessage.RESPTYPE;
-//		    this.message = "XML Error: Received unparseable message";
-//		    throw new KVException(this);
-//		} catch (IOException e) {
-//			this.msgType = KVMessage.RESPTYPE;
-//			this.message = "Network Error: Could not receive data";
-//			throw new KVException(this);
-//		}
+		if (attrs.getLength()!=1)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		Node attr = attrs.item(0);
+		String attrName = attr.getNodeName();
+		String attrValue = attr.getNodeValue();
+		if (!attrName.equals("type"))
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		if (attrValue.equals(KVMessage.PUTTYPE)){
+			checkPutTypeMessage(messageNode);
+		}else if (attrValue.equals(KVMessage.GETTYPE)){
+			checkGetTypeMessage(messageNode);
+		}else if(attrValue.equals(KVMessage.DELTYPE)){
+			checkDelTypeMessage(messageNode);
+		}else if(attrValue.equals(KVMessage.RESPTYPE)){
+			checkRespTypeMessage(messageNode);
+		}else{
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+		}
+		return attrValue;
 		
+	}
+	
+	private void checkRespTypeMessage(Node messageNode) throws KVException{
+		NodeList nodes = messageNode.getChildNodes();
+			
+		if (nodes.getLength()!=1)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		Node key = nodes.item(0);
+		
+		if (!key.getNodeName().equals("Message"))
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+		
+	}
+	
+	private void checkDelTypeMessage(Node messageNode) throws KVException{
+		NodeList nodes = messageNode.getChildNodes();
+			
+		if (nodes.getLength()!=1)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		Node key = nodes.item(0);
+		
+		if (!key.getNodeName().equals("Key"))
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+		
+	}
+	
+	private void checkGetTypeMessage(Node messageNode) throws KVException{
+	NodeList nodes = messageNode.getChildNodes();
+		
+		if (nodes.getLength()!=1)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		Node key = nodes.item(0);
+		
+		if (!key.getNodeName().equals("Key"))
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+	}
+	
+	private void checkPutTypeMessage(Node messageNode) throws KVException{
+		NodeList nodes = messageNode.getChildNodes();
+		
+		if (nodes.getLength()!=2)
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+
+		Node key = nodes.item(0);
+		Node value = nodes.item(1);
+		
+		if (!key.getNodeName().equals("Key") || !value.getNodeName().equals("Value"))
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE, "XML Error: Received unparseable message") );
+	}
+	
+	private void checkDocStructure(Document doc) throws KVException{
+		//doc shoudl have only one child with name KVMessage	
+		if ( !doc.getXmlEncoding().equals("UTF-8") 
+				|| !doc.getXmlVersion().equals("1.0")
+				|| doc.getChildNodes().getLength()!=1 
+				|| !doc.getFirstChild().getNodeName().equals("KVMessage"))
+			throw new KVException( new KVMessage(KVMessage.RESPTYPE,"XML Error: Received unparseable message"));
 	}
 	
 	private boolean isValidMessage(Document dom){
 		return true;
-//		try{
-//			TransformerFactory tf = TransformerFactory.newInstance();
-//			Transformer transformer = tf.newTransformer();
-//			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-//			StringWriter writer = new StringWriter();
-//			transformer.transform(new DOMSource(dom), new StreamResult(writer));
-//			String output = writer.getBuffer().toString().replaceAll("\n|\r", "");
-//			
-//			if ( output.matches("\\<\\?xml version=\"1.0\" encoding=\"UTF-8\"?><KVMessage type=\"getreq\"><Key>%s</Key></KVMessage>")){
-//				
-//			}
-//			
-//			return true;
-//		}catch (Exception e){
-//			return false;
-//		}
 	}
 	
 	/**
@@ -208,20 +319,83 @@ public class KVMessage {
 	 * @throws KVException if not enough data is available to generate a valid KV XML message
 	 */
 	public String toXML() throws KVException {
-	    // TODO: implement me
-		if ( this.msgType.equals(KVMessage.GETTYPE) )
-			return String.format("%s,%s", KVMessage.GETTYPE, this.key);
-//			return String.format(KVMessage.GET_TEMPLATE, this.key);
-		else if( this.msgType .equals( KVMessage.PUTTYPE) )
-			return String.format("%s,%s,%s", KVMessage.PUTTYPE, this.key, this.value);
-//			return String.format(KVMessage.PUT_TEMPLATE, this.key, this.value);
-		else if( this.msgType .equals( KVMessage.DELTYPE ))
-			return String.format("%s,%s", KVMessage.DELTYPE, this.key);
-			//			return String.format(KVMessage.DEL_TEMPLATE, this.key);
-		else if( this.msgType .equals( KVMessage.RESPTYPE))
-			return String.format("%s,%s", KVMessage.RESPTYPE, this.message);
-		else
-			throw new KVException(this);
+    	DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = null;
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			//this should not happen
+			e.printStackTrace();
+		}
+ 
+		// root element
+		Document doc = docBuilder.newDocument();
+		Element rootElement = doc.createElement("KVMessage");
+		rootElement.setAttribute("type", this.msgType);
+		doc.setXmlStandalone(true);
+		doc.appendChild(rootElement);
+		
+		if (this.msgType.equals(KVMessage.PUTTYPE)){
+			if (this.key==null || this.value == null)
+				throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Unknown Error: not enough data to build XML"));
+			if (this.key.length()>KVMessage.MAX_KEY_LENGTH)
+				throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Oversized key"));
+			if (this.value.length() > KVMessage.MAX_VALUE_LENGTH)
+				throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Oversized value"));
+			
+			Element keyElement = doc.createElement("Key");
+			keyElement.appendChild(doc.createTextNode(this.key));
+			
+			Element valElement = doc.createElement("Value");
+			valElement.appendChild(doc.createTextNode(this.value));	
+			
+			rootElement.appendChild(keyElement);
+			rootElement.appendChild(valElement);
+		}else if(this.msgType.equals(KVMessage.GETTYPE) || this.msgType.equals(KVMessage.DELTYPE)){
+			if (this.key==null)
+				throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Unknown Error: not enough data to build XML"));
+			if (this.key.length()>KVMessage.MAX_KEY_LENGTH)
+				throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Oversized key"));
+
+			Element keyElement = doc.createElement("Key");
+			keyElement.appendChild(doc.createTextNode(this.key));
+		
+			rootElement.appendChild(keyElement);
+		}else{
+			if (this.message==null)
+				throw new KVException(new KVMessage(KVMessage.RESPTYPE, "Unknown Error: not enough data to build XML"));
+
+			Element messageElement = doc.createElement("Message");
+			messageElement.appendChild(doc.createTextNode(this.message));
+		
+			rootElement.appendChild(messageElement);	
+		}
+		
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = null;
+		try {
+			transformer = transformerFactory.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			//this should not happen either
+			e.printStackTrace();
+		}
+		
+		StringWriter writer = new StringWriter();
+	
+		DOMSource  source= new DOMSource(doc);
+		StreamResult result = new StreamResult(writer);
+ 
+		
+		try {
+			transformer.transform(source, result);
+		} catch (TransformerException e) {
+			//this should not happen
+			e.printStackTrace();
+		}
+		
+		String kk = writer.toString();
+		return kk;
 			
 	}
 	
@@ -236,58 +410,8 @@ public class KVMessage {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	private static final String GET_TEMPLATE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><KVMessage type=\"getreq\"><Key>%s</Key></KVMessage>";
-	private static final String PUT_TEMPLATE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><KVMessage type=\"putreq\"><Key>%s</Key><Value>%s</Value></KVMessage>";
-	private static final String DEL_TEMPLATE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><KVMessage type=\"putreq\"><Key>%s</Key></KVMessage>";
+	}	
 }
-
-/**
- * Get Value Request:
-<?xml version="1.0" encoding="UTF-8"?>
-<KVMessage type="getreq">
-<Key>key</Key>
-</KVMessage>
-
-Put Value Request:
-<?xml version="1.0" encoding="UTF-8"?>
-<KVMessage type="putreq">
-<Key>key</Key>
-<Value>value</Value>
-</KVMessage>
-
-Delete Value Request:
-<?xml version="1.0" encoding="UTF-8"?>
-<KVMessage type="delreq">
-<Key>key</Key>
-</KVMessage>
-
-Successful Get Response:
-<?xml version="1.0" encoding="UTF-8"?>
-<KVMessage type="resp">
-<Key>key</Key>
-<Value>value</Value>
-</KVMessage>
-
-Successful Put Response:
-<?xml version="1.0" encoding="UTF-8"?>
-<KVMessage type="resp">
-<Message>Success</Message>
-</KVMessage>
-
-Successful Delete Response:
-<?xml version="1.0" encoding="UTF-8"?>
-<KVMessage type="resp">
-<Message>Success</Message>
-</KVMessage>
-
-Unsuccessful Get/Put/Delete Response:
-<?xml version="1.0" encoding="UTF-8"?>
-<KVMessage type="resp">
-<Message>Error Message</Message>
-</KVMessage>
- */
 
 /**
  * XML Parsing library for the key-value store
