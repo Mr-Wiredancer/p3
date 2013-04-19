81a69558dea4d5ae5db6fd3e17cf20e4a1c00eff
Sol Han <sol.h.han@berkeley.edu>
2013-04-18 18:17:02 -0700

    Added some tests, but the program may not run

---
+++ src/edu/berkeley/cs162/KVClientTest.java
@@ -0,0 +1,83 @@
+package edu.berkeley.cs162;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test functionalities of kvstore. You'd better change the delay time in AutoGrader.java before running the tests.
+ * @author amos0528
+ *
+ */
+public class KVClientTest {
+  KVClient client;
+  
+	/**
+	 * set store to a clean state
+	 */
+	@Before
+	public void reset(){
+		client = new KVClient("localhost", 8080);
+	}
+  
+  /** Test successful put. */
+  @Test
+  public void testPut1() {
+    try {
+      client.put("key1", "value1");
+      // inspect KVStore and/or KVCache
+    } catch (KVException e) {
+      fail();
+    }
+  }
+  
+  /** Test multiple puts. */
+  @Test
+  public void testPut2() {
+    try {
+      client.put("key1", "value1");
+      // inspect KVStore and/or KVCache
+      client.put("key1", "value2");
+      assertEquals("value2", client.get("key1"));
+    } catch (KVException e) {
+      fail();
+    }
+  }
+  
+  /** Test successful get. */
+  @Test
+  public void testGet1() {
+    try {
+      client.put("key1", "value1");
+      String result = client.get("key1");
+      assertEquals("value1", result);
+    } catch (KVException e) {
+      fail();
+    }
+  }
+  
+  /** Test get with nonexistent key. */
+  @Test(expected = KVException.class)
+  public void testGet2() throws KVException {
+    String result = client.get("key1");
+  }
+  
+  /** Test successful del. */
+  @Test
+  public void testDel1() {
+    try {
+      client.put("key1", "value1");
+      client.del("key1");
+      // inspect KVStore and/or KVCache
+    } catch (KVException e) {
+      fail();
+    }
+  }
+  
+  /** Test del with nonexistent key. */
+  @Test(expected = KVException.class)
+  public void testDel2() throws KVException {
+    String result = client.delete("key1");
+  }
+}
--- src/edu/berkeley/cs162/ThreadPoolTest.java
+++ src/edu/berkeley/cs162/ThreadPoolTest.java
@@ -1,15 +1,9 @@
-
 package edu.berkeley.cs162;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class KVServerTest {
-
-	@Test
-	public void test() {
-		
-	}
+public class ThreadPoolTest {
 
 }
