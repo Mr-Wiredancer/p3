5dcb1cd40e85858bff63cf9754b5888e244469bd
Jiahao Li <lijiahao90@gmail.com>
2013-04-18 17:10:31 -0700

    deleted unnecessary import in Debuggable.java

--- src/edu/berkeley/cs162/Debuggable.java
+++ src/edu/berkeley/cs162/Debuggable.java
@@ -1,12 +1,5 @@
 package edu.berkeley.cs162;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 public interface Debuggable {
 	
 	public static DefaultDebuggable DEBUG = new DefaultDebuggable();
